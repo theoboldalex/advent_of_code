6e0e6c2465d38c59b7c90533075c3a7faa42a2e3
theoboldalex <theoboldalex@gmail.com>
2024-11-13 21:49:16 +0000

    small refactor

--- day1.clj
+++ day1.clj
@@ -11,17 +11,16 @@ treb7uchet")
 
 (def real-input (slurp "day1.txt"))
 
-(defn get-value-for-line [s]
+(defn get-result-from-line [s]
   (->> s
        (map #(str/replace % #"[^0-9]" ""))
-       (map #(str (first %) (last %)))
-       (map #(Integer/parseInt %))))
+       (map #(Integer/parseInt (str (first %) (last %))))))
 
 (defn get-part-1-answer [s]
   (reduce + (->>
              s
              (str/split-lines)
-             (get-value-for-line))))
+             (get-result-from-line))))
 
 (get-part-1-answer real-input)
 
@@ -68,6 +67,6 @@ zoneight234
              (str/split-lines)
              (map #(replace-word-with-map-value % merged-words-to-num))
              (map #(replace-word-with-map-value % word-to-num))
-             (get-value-for-line))))
+             (get-result-from-line))))
 
 (get-part-2-answer real-input)
