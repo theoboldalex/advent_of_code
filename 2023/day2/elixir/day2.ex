a40e977b1b805878237c1b524b4b1d754b0993f0
theoboldalex <theoboldalex@gmail.com>
2024-11-16 23:38:10 +0000

    Works on test data

--- 2023/day2/elixir/day2.ex
+++ 2023/day2/elixir/day2.ex
@@ -3,6 +3,11 @@ defmodule Day2 do
     input_file
     |> read_input()
     |> Enum.map(&get_line_count_map/1)
+    |> Enum.map(&is_game_legal/1)
+    |> Enum.with_index()
+    |> Enum.filter(fn {v, _} -> v == true end)
+    |> Enum.map(fn {_, index} -> index + 1 end)
+    |> Enum.reduce(&(&1 + &2))
   end
 
   defp read_input(path), do: File.read!("#{__DIR__}/#{path}") |> get_lines()
@@ -25,6 +30,16 @@ defmodule Day2 do
       colour_key = String.to_atom(color)
       Map.update(acc, colour_key, count, &(&1 + count))
     end)
+  end
+
+  defp is_game_legal(game) do
+    limits = %{
+      red: 12,
+      green: 13,
+      blue: 14
+    }
+
+    game[:red] <= limits[:red] and game[:green] <= limits[:green] and game[:blue] <= limits[:blue]
   end
 end
 
