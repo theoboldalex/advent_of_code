fe9ab55844e91ab2a678619435e3e2c7cc136992
theoboldalex <theoboldalex@gmail.com>
2024-11-17 19:31:42 +0000

    Day 2 part 2

--- 2023/day2/elixir/day2.ex
+++ 2023/day2/elixir/day2.ex
@@ -7,6 +7,16 @@ defmodule Day2 do
     |> Enum.filter(fn {v, _} -> not Enum.member?(v, false) end)
     |> Enum.map(fn {_, index} -> index + 1 end)
     |> Enum.reduce(&(&1 + &2))
+    |> IO.inspect()
+  end
+
+  def part_two_answer(input_file) do
+    input_file
+    |> read_input()
+    |> Enum.map(&get_min_cubes/1)
+    |> Enum.map(&get_max_from_game/1)
+    |> Enum.reduce(&(&1 + &2))
+    |> IO.inspect()
   end
 
   defp read_input(path), do: File.read!("#{__DIR__}/#{path}") |> get_lines()
@@ -44,4 +54,35 @@ defmodule Day2 do
 
     game[:red] <= limits[:red] and game[:green] <= limits[:green] and game[:blue] <= limits[:blue]
   end
+
+  defp get_min_cubes(line) do
+    games = String.split(line, ";")
+    pattern = ~r/([0-9]+) (red|green|blue)/
+
+    Enum.map(games, fn game ->
+      matches = Regex.scan(pattern, game)
+
+      game = %{
+        red: 0,
+        green: 0,
+        blue: 0
+      }
+
+      Enum.reduce(matches, game, fn [_, count, colour], acc ->
+        count = String.to_integer(count)
+        key = String.to_atom(colour)
+        Map.update(acc, key, count, &(&1 + count))
+      end)
+    end)
+  end
+
+  defp get_max_from_game(game) do
+    red = game |> Enum.map(fn map -> map[:red] end)
+    green = game |> Enum.map(fn map -> map[:green] end)
+    blue = game |> Enum.map(fn map -> map[:blue] end)
+
+    Enum.max(red) * Enum.max(green) * Enum.max(blue)
+  end
 end
+
+Day2.part_one_answer("real.txt")
