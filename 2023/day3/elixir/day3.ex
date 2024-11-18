d1849e54036ac912cb270584eb79d665d22eb33a
theoboldalex <theoboldalex@gmail.com>
2024-11-18 09:16:04 +0000

    Gets chunks of input

--- 2023/day3/elixir/day3.ex
+++ 2023/day3/elixir/day3.ex
@@ -1,5 +1,13 @@
 defmodule Day3 do
+  def part_one(input) do
+    input
+    |> read_input()
+    |> Enum.chunk_every(2, 1, :discard)
+  end
+
   defp read_input(path), do: File.read!("#{__DIR__}/#{path}") |> get_lines()
 
   defp get_lines(str), do: str |> String.trim() |> String.split(~r{\n})
 end
+
+IO.inspect(Day3.part_one("test.txt"))
