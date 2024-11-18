f19d8d56a63f142c2b2d6ece3ed92a1411c4100f
theoboldalex <theoboldalex@gmail.com>
2024-11-18 09:58:08 +0000

    gets graphemes of each line of a chunk

--- 2023/day3/elixir/day3.ex
+++ 2023/day3/elixir/day3.ex
@@ -3,11 +3,29 @@ defmodule Day3 do
     input
     |> read_input()
     |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(&get_part_numbers/1)
   end
 
-  defp read_input(path), do: File.read!("#{__DIR__}/#{path}") |> get_lines()
+  defp get_part_numbers(chunk) do
+    chunk
+    |> Enum.map(fn s ->
+      s
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.map(fn {k, v} ->
+        {k, v}
+      end)
+    end)
+  end
+
+  defp is_special_char?(char), do: char not in ["."] and not is_number?(char)
+
+  defp is_number?(char), do: Regex.match?(~r/^\d$/, char)
+
+  defp(read_input(path), do: File.read!("#{__DIR__}/#{path}") |> get_lines())
 
   defp get_lines(str), do: str |> String.trim() |> String.split(~r{\n})
 end
 
-IO.inspect(Day3.part_one("test.txt"))
+Day3.part_one("test.txt")
+|> IO.inspect()
