b632594d0061398fe31434eba67d9c73e5751a88
theoboldalex <theoboldalex@gmail.com>
2024-12-01 18:55:52 +0000

    day two setup

---
+++ 2024/elixir/day2.ex
@@ -0,0 +1,14 @@
+defmodule DayTwo do
+  @input_path "test.txt"
+
+  def part_one do
+    nil
+  end
+
+  def part_two do
+    nil
+  end
+end
+
+IO.puts("part one: #{DayTwo.part_one()}")
+IO.puts("part two: #{DayTwo.part_two()}")
