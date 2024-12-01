085b72c3e6fd01663afe7fa7f9a87774358aa5a0
theoboldalex <theoboldalex@gmail.com>
2024-12-01 10:31:49 +0000

    part two

--- 2024/elixir/day1.ex
+++ 2024/elixir/day1.ex
@@ -1,25 +1,24 @@
 defmodule DayOne do
-  @input_path "real.txt"
+  @input_path "test.txt"
 
-  def stream_file do
-    File.stream!("#{__DIR__}/#{@input_path}")
-  end
+  def stream_file, do: File.stream!("#{__DIR__}/#{@input_path}")
 
-  def get_line_parts(line) do
-    line
-    |> String.trim()
-    |> String.split(~r/\s+/, parts: 2)
+  def get_line_parts do
+    stream_file()
+    |> Enum.map(fn l ->
+      l
+      |> String.trim()
+      |> String.split(~r/\s+/, parts: 2)
+    end)
   end
 
   def get_left do
-    stream_file()
-    |> Enum.map(&get_line_parts/1)
+    get_line_parts()
     |> Enum.map(&String.to_integer(hd(&1)))
   end
 
   def get_right do
-    stream_file()
-    |> Enum.map(&get_line_parts/1)
+    get_line_parts()
     |> Enum.map(fn l ->
       case l do
         [_, tail] -> String.to_integer(tail)
@@ -28,12 +27,20 @@ defmodule DayOne do
     end)
   end
 
-  def result do
+  def part_one do
     Enum.zip(Enum.sort(get_left()), Enum.sort(get_right()))
     |> Enum.map(fn {l, r} -> abs(l - r) end)
     |> Enum.reduce(&(&1 + &2))
   end
+
+  def part_two do
+    left = get_left()
+    right = Enum.frequencies(get_right())
+
+    Enum.map(left, &(&1 * Map.get(right, &1, 0)))
+    |> Enum.reduce(&(&1 + &2))
+  end
 end
 
-DayOne.result()
-|> IO.inspect()
+IO.puts("part one: #{DayOne.part_one()}")
+IO.puts("part two: #{DayOne.part_two()}")
