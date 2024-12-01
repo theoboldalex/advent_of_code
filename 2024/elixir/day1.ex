4aa87e394b68b0005377f59990c6b761c343ee48
theoboldalex <theoboldalex@gmail.com>
2024-12-01 19:34:46 +0000

    make parser private

--- 2024/elixir/day1.ex
+++ 2024/elixir/day1.ex
@@ -1,17 +1,6 @@
 defmodule DayOne do
   @input_path "test.txt"
 
-  def get_sides do
-    File.read!("#{__DIR__}/#{@input_path}")
-    |> String.split(~r/\n/, trim: true)
-    |> Enum.map(fn l ->
-      String.split(l)
-      |> Enum.map(&String.to_integer/1)
-      |> List.to_tuple()
-    end)
-    |> Enum.unzip()
-  end
-
   def part_one do
     {left, right} = get_sides()
 
@@ -29,6 +18,17 @@ defmodule DayOne do
     Enum.reduce(left, 0, fn n, acc ->
       n * Map.get(freq, n, 0) + acc
     end)
+  end
+
+  defp get_sides do
+    File.read!("#{__DIR__}/#{@input_path}")
+    |> String.split(~r/\n/, trim: true)
+    |> Enum.map(fn l ->
+      String.split(l)
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+    |> Enum.unzip()
   end
 end
 
