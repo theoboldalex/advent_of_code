a4f5de256631fc4f73299ec8b8bbadee164fb981
theoboldalex <theoboldalex@gmail.com>
2024-12-02 09:22:48 +0000

    part one complete

--- 2024/elixir/day2.ex
+++ 2024/elixir/day2.ex
@@ -1,5 +1,5 @@
 defmodule DayTwo do
-  @input_path "test.txt"
+  @input_path "real.txt"
 
   def get_lines do
     File.read!("#{__DIR__}/#{@input_path}")
@@ -8,20 +8,31 @@ defmodule DayTwo do
     |> Enum.map(fn line ->
       String.split(line)
       |> Enum.map(&String.to_integer/1)
-      |> List.to_tuple()
     end)
   end
 
   def part_one do
     get_lines()
-    |> IO.inspect()
+    |> Enum.filter(&is_report_safe?/1)
+    |> length()
   end
 
   def part_two do
     nil
   end
+
+  defp is_report_safe?(report), do: asc?(report) or desc?(report)
+
+  defp asc?([]), do: true
+  defp asc?([_]), do: true
+  defp asc?([a, b | rest]) when a < b and b - a <= 3, do: asc?([b | rest])
+  defp asc?(_), do: false
+
+  defp desc?([]), do: true
+  defp desc?([_]), do: true
+  defp desc?([a, b | rest]) when a > b and a - b <= 3, do: desc?([b | rest])
+  defp desc?(_), do: false
 end
 
-# IO.puts("part one: #{DayTwo.part_one()}")
-# IO.puts("part two: #{DayTwo.part_two()}")
-DayTwo.part_one()
+IO.puts("part one: #{DayTwo.part_one()}")
+IO.puts("part two: #{DayTwo.part_two()}")
