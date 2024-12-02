3a1776d832c2028950f7a1be91b3362dd2dbc66c
theoboldalex <theoboldalex@gmail.com>
2024-12-02 08:56:02 +0000

    parse input into lines

--- 2024/elixir/day2.ex
+++ 2024/elixir/day2.ex
@@ -1,8 +1,20 @@
 defmodule DayTwo do
   @input_path "test.txt"
 
+  def get_lines do
+    File.read!("#{__DIR__}/#{@input_path}")
+    |> String.trim()
+    |> String.split(~r/\n/)
+    |> Enum.map(fn line ->
+      String.split(line)
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+  end
+
   def part_one do
-    nil
+    get_lines()
+    |> IO.inspect()
   end
 
   def part_two do
@@ -10,5 +22,6 @@ defmodule DayTwo do
   end
 end
 
-IO.puts("part one: #{DayTwo.part_one()}")
-IO.puts("part two: #{DayTwo.part_two()}")
+# IO.puts("part one: #{DayTwo.part_one()}")
+# IO.puts("part two: #{DayTwo.part_two()}")
+DayTwo.part_one()
