667226bc88d67591e8951ba2a6f805d57ea0d9bd
theoboldalex <theoboldalex@gmail.com>
2024-12-03 14:48:59 +0000

    refactor for un-needed duplication

--- 2024/elixir/day3.ex
+++ 2024/elixir/day3.ex
@@ -6,9 +6,7 @@ defmodule DayThree do
   end
 
   def part_two do
-    parts = Regex.split(~r/do\(\)|don't\(\)/, read_input(), include_captures: true)
-    # because the start of the input has no do() call, save the head for later
-    [hd | tail] = parts
+    [hd | tail] = Regex.split(~r/do\(\)|don't\(\)/, read_input(), include_captures: true)
 
     do_blocks =
       Enum.chunk_every(tail, 2)
@@ -24,7 +22,6 @@ defmodule DayThree do
 
   defp sum_matches(contents) do
     pattern = ~r/mul\((\d{1,3}),(\d{1,3})\)/
-
     matches = Regex.scan(pattern, contents)
 
     Enum.reduce(matches, 0, fn [_, l, r], acc ->
