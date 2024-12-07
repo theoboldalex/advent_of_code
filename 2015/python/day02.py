3368e6c4e933f537835231f22cc6fba3ef95af29
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-12-07 23:41:50 +0000

    day 2 part 2

--- 2015/python/day02.py
+++ 2015/python/day02.py
@@ -12,13 +12,22 @@ with open("input.txt", "r") as file:
 
         total += (2 * sum(sides)) + slack
 
-
 print(total)
 
-
-
-
-
-
-
 # part two
+def array_product(arr):
+    product = 1
+    for i in arr:
+        product *= i
+    return product
+
+total = 0
+with open("input.txt", "r") as file:
+    for line in file:
+        parts = [int(i) for i in line.strip().split("x")]
+        ribbon_for_bow = array_product(parts)
+        parts.remove(max(parts))
+        ribbon_for_present = 2 * sum(parts)
+        total += ribbon_for_bow + ribbon_for_present
+
+print(total)
