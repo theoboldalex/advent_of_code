eb7cd80dace1a91d8bd3833fdca6da9ddd0019db
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-12-08 23:48:36 +0000

    simplifies to use tuple to avoid conversion from dict

--- 2015/python/day03.py
+++ 2015/python/day03.py
@@ -1,28 +1,26 @@
 # part one
 contents = open("input.txt", "r").read().strip()
 
-coords_visited = [{"x": 0, "y": 0}]
+coords_visited = [(0, 0)]
 
 for char in contents:
-    current_postion = {}
-    last_position = coords_visited[-1]
     if char == "^":
-        current_postion["x"] = last_position["x"]
-        current_postion["y"] = last_position["y"] + 1
+        coords_visited.append(
+            (coords_visited[-1][0], coords_visited[-1][1] + 1)
+        )
     elif char == "v":
-        current_postion["x"] = last_position["x"]
-        current_postion["y"] = last_position["y"] - 1
+        coords_visited.append(
+            (coords_visited[-1][0], coords_visited[-1][1] - 1)
+        )
     elif char == ">":
-        current_postion["x"] = last_position["x"] + 1
-        current_postion["y"] = last_position["y"]
+        coords_visited.append(
+            (coords_visited[-1][0] + 1, coords_visited[-1][1])
+        )
     elif char == "<":
-        current_postion["x"] = last_position["x"] - 1
-        current_postion["y"] = last_position["y"]
+        coords_visited.append(
+            (coords_visited[-1][0] - 1, coords_visited[-1][1])
+        )
 
-    coords_visited.append(current_postion)
-
-visited = [dict(t) for t in {tuple(d.items()) for d in coords_visited}]
-
-print(len(visited))
+print(len(set(coords_visited)))
         
         
