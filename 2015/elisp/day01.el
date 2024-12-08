18e127b7ffefbebcbae1e8fef9225c737945dd4f
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-12-08 10:07:45 +0000

    improved approach with seq-reduce and cond

--- 2015/elisp/day01.el
+++ 2015/elisp/day01.el
@@ -4,10 +4,23 @@
     (insert-file-contents-literally "input.txt")
     (split-string (buffer-string) "")))
 
-(setq climb (length (seq-filter (lambda (c) (string-match c "(")) file-contents)))
-(setq descend (length (seq-filter (lambda (c) (string-match c ")")) file-contents)))
+;; first approach
+;;(setq climb (length (seq-filter (lambda (c) (string-match c "(")) file-contents)))
+;;(setq descend (length (seq-filter (lambda (c) (string-match c ")")) file-contents)))
+;;(print (concat "part one: " (number-to-string (- climb descend))))
 
-(print (concat "part one: " (number-to-string (- climb descend))))
+;; better approach
+(setq answer-to-part-one
+      (seq-reduce
+       (lambda (acc char)
+	 (cond
+	  ((string= char "(") (+ acc 1))
+	  ((string= char ")") (- acc 1))
+	  (t acc)))
+       file-contents
+       0))
+
+(print (concat "part one: " (number-to-string answer-to-part-one)))
 
 ;; part two
 
