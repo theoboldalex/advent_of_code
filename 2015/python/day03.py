67c7b620e0188470b2e8958745c59ab6e06c6376
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-12-09 17:05:45 +0000

    straight up junior dev code

--- 2015/python/day03.py
+++ 2015/python/day03.py
@@ -24,3 +24,46 @@ for char in contents:
 print(len(set(coords_visited)))
 
 # part two
+
+coords_visited = {
+    "santa": [(0, 0)],
+    "robot": [(0, 0)]
+}
+
+for (idx, char) in enumerate(contents):
+    if idx % 2 == 0:
+        if char == "^":
+            coords_visited["santa"].append(
+                (coords_visited["santa"][-1][0], coords_visited["santa"][-1][1] + 1)
+            )
+        elif char == "v":
+            coords_visited["santa"].append(
+                (coords_visited["santa"][-1][0], coords_visited["santa"][-1][1] - 1)
+            )
+        elif char == ">":
+            coords_visited["santa"].append(
+                (coords_visited["santa"][-1][0] + 1, coords_visited["santa"][-1][1])
+            )
+        elif char == "<":
+            coords_visited["santa"].append(
+                (coords_visited["santa"][-1][0] - 1, coords_visited["santa"][-1][1])
+            )
+    else:
+        if char == "^":
+            coords_visited["robot"].append(
+                (coords_visited["robot"][-1][0], coords_visited["robot"][-1][1] + 1)
+            )
+        elif char == "v":
+            coords_visited["robot"].append(
+                (coords_visited["robot"][-1][0], coords_visited["robot"][-1][1] - 1)
+            )
+        elif char == ">":
+            coords_visited["robot"].append(
+                (coords_visited["robot"][-1][0] + 1, coords_visited["robot"][-1][1])
+            )
+        elif char == "<":
+            coords_visited["robot"].append(
+                (coords_visited["robot"][-1][0] - 1, coords_visited["robot"][-1][1])
+            )
+
+print(len(set(coords_visited["santa"] + coords_visited["robot"])))
