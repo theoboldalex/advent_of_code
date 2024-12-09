7caac9fd03d2b4007ec0c51ff287ba111fe6b0f4
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-12-09 14:46:27 +0000

    elisp day three part one

---
+++ 2015/elisp/day03.el
@@ -0,0 +1,42 @@
+(setq input
+      (with-temp-buffer
+	(insert-file-contents-literally "input.txt")
+	(string-trim (buffer-string))))
+
+;; part one
+(setq coords-visited (list (list 0 0))) ;; initial setup here so in scope for functions
+
+(defun build-path (str)
+  (setq coords-visited (list (list 0 0))) ;; reset here otherwise answer only correct first time run interactively
+  (seq-map
+   'set-current-coords
+   str))
+
+(defun set-current-coords (char)
+  (let ((previous-coords (seq-first coords-visited)))
+    (cond
+     ((string= (char-to-string char) "^")
+      (push
+       (list (seq-first previous-coords) (1+ (car (last previous-coords))))
+       coords-visited))
+     ((string= (char-to-string char) "v")
+      (push
+       (list (seq-first previous-coords) (1- (car (last previous-coords))))
+       coords-visited))
+     ((string= (char-to-string char) ">")
+      (push
+       (list (1+ (seq-first previous-coords)) (car (last previous-coords)))
+       coords-visited))
+     ((string= (char-to-string char) "<")
+      (push
+       (list (1- (seq-first previous-coords)) (car (last previous-coords)))
+       coords-visited))
+     (t nil))))
+
+(print
+ (concat
+  "part one: "
+  (number-to-string
+   (length
+    (delete-dups
+     (car (last (build-path input))))))))
