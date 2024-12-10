63a83fcebfc92415000eede7aa169e976bb76673
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-12-10 00:18:56 +0000

    day 4 part 2

--- 2015/python/day04.py
+++ 2015/python/day04.py
@@ -19,3 +19,16 @@ def part_one(puzzle_input):
 print(part_one(puzzle_input))
 
 # part two
+def answer_starts_with_six_zeroes(hex):
+    return hex[:6] == "000000"
+
+def part_two(puzzle_input):
+    answer = 1
+    while True:
+        current_guess = f"{puzzle_input}{answer}"
+        hash = hashlib.md5(current_guess.encode("UTF-8")).hexdigest()
+        if answer_starts_with_six_zeroes(hash):
+            return answer
+        answer += 1
+
+print(part_two(puzzle_input))
