8dcc0f3914be99464cc351c36b718a0c63c9461e
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-12-10 00:14:44 +0000

    day 4 part 1

---
+++ 2015/python/day04.py
@@ -0,0 +1,21 @@
+import hashlib
+
+# part one
+puzzle_input = "iwrupvqb"
+answer_found = False
+
+def answer_starts_with_five_zeroes(hex):
+    return hex[:5] == "00000"
+
+def part_one(puzzle_input):
+    answer = 1
+    while True:
+        current_guess = f"{puzzle_input}{answer}"
+        hash = hashlib.md5(current_guess.encode("UTF-8")).hexdigest()
+        if answer_starts_with_five_zeroes(hash):
+            return answer
+        answer += 1
+
+print(part_one(puzzle_input))
+
+# part two
