c3c207d3bc5aab13bfba1999725fef11d5ac1987
Alex Theobold <theoboldalex@gmail.com>
2025-12-10 21:34:11 +0000

    day 2 part 2

--- 2025/python/day_02.py
+++ 2025/python/day_02.py
@@ -12,4 +12,16 @@ def part_one():
 
         return sum(invalid)
 
+def part_two():
+    with open("./input/day_02.txt") as input:
+        invalid = []
+        id_ranges = [tuple(i.split("-")) for i in input.read().strip().split(",")]
+
+        for min, max in id_ranges:
+            for i in range(int(min), int(max)):
+                if re.search(r"^(\d+)\1+$", str(i)): invalid.append(i)
+
+        return sum(invalid)
+
 print(f"part one: {part_one()}")
+print(f"part two: {part_two()}")
