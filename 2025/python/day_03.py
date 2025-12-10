93abfaad26babafac5b38fde0b84090f42ae1c95
Alex Theobold <theoboldalex@gmail.com>
2025-12-10 22:10:12 +0000

    casing

--- 2025/python/day_03.py
+++ 2025/python/day_03.py
@@ -5,18 +5,18 @@ def part_one():
     with open("./input/day_03.txt") as input:
         for row in input:
             cleaned_row = row.strip()
-            maxJoltageInBank = 0
+            max_joltage_in_bank = 0
 
             for i in range(len(cleaned_row)):
                 for j in range(i + 1, len(cleaned_row)):
                     current = int(f"{cleaned_row[i]}{cleaned_row[j]}")
                     if current == max_possible_joltage:
-                        maxJoltageInBank = max_possible_joltage
+                        max_joltage_in_bank = max_possible_joltage
                         continue
 
-                    if current > maxJoltageInBank: maxJoltageInBank = current
+                    if current > max_joltage_in_bank: max_joltage_in_bank = current
 
-            total_output_joltage += maxJoltageInBank
+            total_output_joltage += max_joltage_in_bank
         
         return total_output_joltage
                 
