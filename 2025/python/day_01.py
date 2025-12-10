ae5f3e472a6d2710dce0d8fbc2c2bd164fa93422
Alex Theobold <theoboldalex@gmail.com>
2025-12-10 20:47:16 +0000

    part two

--- 2025/python/day_01.py
+++ 2025/python/day_01.py
@@ -9,19 +9,31 @@ def part_one():
             direction = clean_row[0]
             distance = int(clean_row[-1] if len(row) <= 3 else clean_row[-2:])
 
-            if direction == "L":
-                current_pos -= distance
-                if current_pos < 0: current_pos += 100
-            else:
-                current_pos += distance
-                if current_pos > 99: current_pos -= 100
-
+            current_pos = current_pos + distance if direction == "L" else current_pos - distance
+            if current_pos > 99: current_pos -= 100
+            elif current_pos < 0: current_pos += 100
             if current_pos == 0: answer += 1
 
     return answer
 
 def part_two():
-    return 2
+    start_pos = 50
+    current_pos = start_pos
+    answer = 0
+
+    with open("./input/day_01.txt") as input:
+        for row in input:
+            clean_row = row.strip()
+            direction = clean_row[0]
+            distance = int(clean_row[1:])
+
+            for i in range(distance):
+                current_pos = current_pos + 1 if direction == "L" else current_pos - 1
+                if current_pos > 99: current_pos -= 100
+                elif current_pos < 0: current_pos += 100
+                if current_pos == 0: answer += 1 
+
+    return answer
 
 print(f"part one: {part_one()}")
 print(f"part two: {part_two()}")
