5c587070ea9e23d6d79d70a790c92dcafe14c055
Alex Theobold <theoboldalex@gmail.com>
2025-12-22 22:35:06 +0000

    day 5 part 2

--- 2025/python/day_05.py
+++ 2025/python/day_05.py
@@ -6,8 +6,7 @@ def part_one():
     with open("./input/day_05.txt") as input:
         for line in map(str.strip, input):
             if "-" in line:
-                number_range = line.split("-")
-                ingredient_ranges.append([int(i) for i in number_range])
+                ingredient_ranges.append([int(i) for i in line.split("-")])
             elif line:
                 available_ingredients.append(int(line))
 
@@ -19,4 +18,48 @@ def part_one():
 
     return fresh_ingredients
 
+def part_two():
+    """probably not a good idea to run this one unless you have a few years to spare"""
+    ingredient_ranges = []
+    fresh_ingredients = []
+
+    with open("./input/day_05.txt") as input:
+        for line in map(str.strip, input):
+            if "-" in line:
+                number_range = [int(i) for i in line.split("-")]
+                for i in range(number_range[0], number_range[1] + 1):
+                    if i not in fresh_ingredients:
+                        fresh_ingredients.append(i)
+
+    return len(fresh_ingredients)
+
+def part_two_efficient():
+    ingredient_ranges = []
+    fresh_ingredients = 0
+
+    with open("./input/day_05.txt") as input:
+        for line in map(str.strip, input):
+            if "-" in line:
+                ingredient_ranges.append([int(i) for i in line.split("-")])
+
+    ingredient_ranges.sort()
+    stack = []
+    merged_ranges = []
+    for i in range(len(ingredient_ranges)):
+        current = ingredient_ranges[i]
+        if not stack or stack[-1][1] < current[0]:
+            stack.append(current)
+        elif stack[-1][1] < current[1]:
+            stack[-1][1] = current[1]
+
+    while stack:
+        merged_ranges.append(stack.pop())
+
+    for i in merged_ranges:
+        fresh_ingredients += (i[1] - i[0]) + 1
+
+    return fresh_ingredients
+
 print(f"Part one: {part_one()}")
+# print(f"Part two: {part_two()}")
+print(f"Part two efficient: {part_two_efficient()}")
