047d5866a1331e4105c4f2760f7b2cf7a2ae122a
Alex Theobold <theoboldalex@gmail.com>
2025-12-24 18:41:30 +0000

    pass

--- 2025/python/day_07.py
+++ 2025/python/day_07.py
@@ -21,22 +21,7 @@ def part_one():
     return splits
 
 def part_two():
-    splits = 0
-    beams = set([])
-
-    with open(INPUT_PATH, "r") as input:
-        for idx, line in enumerate(map(str.strip, input)):
-            if START_CHAR in line:
-                beams.add(line.index(START_CHAR))
-                continue
-
-            for idx, char in enumerate(line):
-                if char == BEAM_SPLITTER_CHAR and idx in beams:
-                    splits += 1
-                    beams.discard(idx)
-                    beams.add(idx + 1)
-                    beams.add(idx - 1)
-    return splits
+    pass
 
 print(f"Part one ⭐: {part_one()}")
 print(f"Part two: {part_two()}")
